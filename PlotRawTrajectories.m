6e6375304454f0214714c56720f7b87cc343d2ed
najamsakib <91304443+najamsakib@users.noreply.github.com>
2021-10-04 19:46:26 -0400

    Update PlotRawTrajectories.m
    3 plots for P1 plotted

--- PlotRawTrajectories.m
+++ PlotRawTrajectories.m
@@ -10,10 +10,42 @@
 % =========================================================================
 function success = PlotRawTrajectories(obj)
     try
-        disp(obj.SillyMessage);
-        success = true;
+         %disp(obj.SillyMessage);
+         % success = true;
+         time_data = Analysis.A(:,1);
+         xdata = Analysis.A(:,2:3:31);
+         ydata = Analysis.A(:,3:3:31);
+         zdata = Analysis.A(:,4:3:31);
+         
+         for i = 1:10
+            subplot(2,2,1)
+            plot(time_data,xdata(:,i), 'linewidth', 2)
+            xlabel('t')
+            ylabel('x')
+            hold on
+            subplot(2,2,2)
+            plot(time_data,ydata(:,i), 'linewidth', 2)
+            xlabel('t')
+            ylabel('y')
+            hold on
+            subplot(2,2,3)
+            plot(time_data,zdata(:,i), 'linewidth', 2)
+            xlabel('t')
+            ylabel('z')
+            hold on
+
+        end
+        %text(1, 1, 1, 'T=0s' )
+        %text(12, 20, 1, 'T=end' )
+        %legend('Run 1', 'Run 2', 'Run 3', 'Run 4', 'Run 5', ...
+        %       'Run 6', 'Run 7', 'Run 8', 'Run 9', 'Run 10' )
+
+        %grid on
     catch
         warning('Something went wrong in running PlotRawTrajectories!');
         success = false;
     end
 end
+
+
+
